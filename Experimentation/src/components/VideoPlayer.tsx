13ccd5c4160d73463ef0b88989bba2b089fa4273
t-apunjabi <108371617+t-apunjabi@users.noreply.github.com>
2022-06-28 16:12:20 -0700

    Update VideoPlayer.tsx

--- Experimentation/src/components/VideoPlayer.tsx
+++ Experimentation/src/components/VideoPlayer.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactPlayer from '../react-player/youtube'
+//import ReactPlayer from '../react-player/youtube'
 function VideoPlayer(props: { url: string, settings:any }) {
     return (
         <>
-        <ReactPlayer url={props.url} playing={props.settings.autoPlay} playbackRate={props.settings.playbackRate} volume={props.settings.volume} style={{
+            {/*<ReactPlayer url={props.url} playing={props.settings.autoPlay} playbackRate={props.settings.playbackRate} volume={props.settings.volume} style={{
                 position: 'relative', marginLeft: 'auto',
-                marginRight: 'auto', marginBottom: '25px'
+                marginRight: 'auto', marginBottom: '25px'*/}
         }} />
       </>
   );
